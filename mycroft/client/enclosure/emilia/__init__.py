bde1c35b5cde622c8bf9e3868d72a1e02c5301ba
Marcello Yesca <yesca@outlook.com>
2020-02-24 18:32:10 -0300

    Printer refactoring

--- mycroft/client/enclosure/emilia/__init__.py
+++ mycroft/client/enclosure/emilia/__init__.py
@@ -29,15 +29,17 @@ from mycroft.messagebus.message import Message
 from mycroft.util import connected
 from mycroft.util.log import LOG
 from queue import Queue
-import OPi.GPIO as GPIO
+import gpiod #Pi.GPIO as GPIO
 from enum import Flag, auto
 
-pBusy = 5               # parallel busy  PA11
-pLatch = 3              # parallel latch PA12
-sClock = 23             # serial clock   PA14
-sData = 19              # serial data    PA15
-sLatch = 21             # serial latch   PA16
+pBusy = 11 #5               # parallel busy  PA11
+pLatch = 12 #3              # parallel latch PA12
+sClock = 14 #23             # serial clock   PA14
+sData = 15 #19              # serial data    PA15
+sLatch = 16 #21             # serial latch   PA16
 
+GPIO_HIGH = 1
+GPIO_LOW = 0
 
 class PrinterCommand(Flag):
     RESET = auto()
@@ -76,53 +78,57 @@ class EnclosurePrinter(Thread):
 
     def flush(self):
         try:
-            GPIO.setmode(GPIO.BOARD)
-            GPIO.setup(pBusy, GPIO.IN)
-            GPIO.setup(pLatch, GPIO.OUT, GPIO.HIGH)
-            GPIO.setup(sClock, GPIO.OUT, GPIO.LOW)
-            GPIO.setup(sData, GPIO.OUT, GPIO.LOW)
-            GPIO.setup(sLatch, GPIO.OUT, GPIO.HIGH)
 
-            while self.alive:
-                try:
-                    chunck = self.chuncks.get()
-                    self.__write__(chunck)
-                    self.chuncks.task_done()
-                except Exception as e:
-                    LOG.error("Unqueueing error: {0}".format(e))
+            with gpiod.Chip("gpiochip0") as chip:
+                lines = {
+                        "pBusy": chip.get_line(pBusy),
+                        "sData": chip.get_line(sData),
+                        "sClock": chip.get_line(sClock),
+                        "sLatch": chip.get_line(sLatch),
+                        "pLatch": chip.get_line(pLatch)
+                        }
+                lines["pBusy"].request(consumer="emilia", type=gpiod.LINE_REQ_DIR_IN)
+                lines["sData"].request(consumer="emilia", type=gpiod.LINE_REQ_DIR_OUT)
+                lines["sClock"].request(consumer="emilia", type=gpiod.LINE_REQ_DIR_OUT)
+                lines["sLatch"].request(consumer="emilia", type=gpiod.LINE_REQ_DIR_OUT)
+                lines["pLatch"].request(consumer="emilia", type=gpiod.LINE_REQ_DIR_OUT)
+
+                while self.alive:
+                    try:
+                        chunck = self.chuncks.get()
+                        self.__write__(lines, chunck)
+                        self.chuncks.task_done()
+                    except Exception as e:
+                        LOG.error("Unqueueing error: {0}".format(e))
         finally:
-            GPIO.output(sData, GPIO.LOW)
-            GPIO.output(sClock, GPIO.LOW)
-            GPIO.output(sLatch, GPIO.LOW)
-            GPIO.output(pLatch, GPIO.HIGH)
-            GPIO.cleanup()
+            pass
 
-    def __write__(self, chunck):
+    def __write__(self, lines, chunck):
         try:
             for byte in chunck:
-                self.__writeByte__(byte)
+                self.__writeByte__(lines, byte)
                 sleep(0.01)
         except Exception as e:
             LOG.error("Writing error: {0}".format(e))
 
-    def __writeByte__(self, value):   # MSB out first!
-        while GPIO.input(pBusy) == GPIO.HIGH:
+    def __writeByte__(self, lines, value):   # MSB out first!
+        while lines["pBusy"].get_value() == GPIO_HIGH:
             sleep(0.01)
         for x in range(0, 8):
             temp = value & 0x80
             if temp == 0x80:
-                GPIO.output(sData, GPIO.HIGH)
+                lines["sData"].set_value(GPIO_HIGH)
             else:
-                GPIO.output(sData, GPIO.LOW)
-            GPIO.output(sClock, 1)
-            GPIO.output(sClock, 0)
+                lines["sData"].set_value(GPIO_LOW)
+            lines["sClock"].set_value(GPIO_HIGH)
+            lines["sClock"].set_value(GPIO_LOW)
             value = value << 0x01                        # shift left
-        GPIO.output(sLatch, GPIO.LOW)
+        lines["sLatch"].set_value(GPIO_LOW)
         sleep(0.001)
-        GPIO.output(sLatch, GPIO.HIGH)
-        GPIO.output(pLatch, GPIO.LOW)
+        lines["sLatch"].set_value(GPIO_HIGH)
+        lines["pLatch"].set_value(GPIO_LOW)
         sleep(0.001)
-        GPIO.output(pLatch, GPIO.HIGH)
+        lines["pLatch"].set_value(GPIO_HIGH)
 
     def print(self, chunck):
         self.chuncks.put(chunck)
